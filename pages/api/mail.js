e14a20787cf2a40d4facc9aa86970b4aef46e340
Bryan Thai <rawgaming3@gmail.com>
2022-08-04 18:31:55 -0500

    Set up contact form with sendgrid

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,6 +1,25 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default (req, res) => {
     const body = JSON.parse(req.body);
-    console.log(body);
+    
+    const message = `
+        Name: ${body.name}\r\n
+        Email:${body.email}\r\n
+        Message: ${body.message}   
+    `;
+
+    const data = {
+        to:'rawgaming3@gmail.com',
+        from: 'email@bryanthai.com',
+        subject: 'New web form message!',
+        test:message,
+        html: message.replace(/\r\n/g,'<br>')
+    };
+
+    mail.send(data)
+
     res.status(200).json({status:'OK'});
 }
